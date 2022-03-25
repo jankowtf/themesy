d274587b3d89debfad495f6e1c479e5b0b380040
Janko Thyson <janko.thyson@rappster.io>
2022-03-25 19:06:27 +0100

    v0.0.0.9002
    - Added and changed colors to/in `dbt_001_colors()`

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# themesy 0.0.0.9002
+
+- Added and changed colors to/in `dbt_001_colors()`
+
 # themesy 0.0.0.9001
 
 - Added colors to `dbt_001_colors()`
--- R/themes.R
+++ R/themes.R
@@ -19,22 +19,52 @@ dbt_001_colors <- function(
     tbl <- tibble::tribble(
         ~name, ~desc, ~value,
         "one", "red-500", "#f01314",
-        "two", "blue-500", "#1455c0",
-        "three", "cyan-500", "#ffd800",
         "one-light", "red-400", "#f75056",
-        "two-light", "blue-400", "#347de0",
-        "three-light", "cyan-400", "#55b9e6",
         "one-dark", "red-600", "#c50014",
+
+        "two", "blue-500", "#1455c0",
+        "two-light", "blue-400", "#347de0",
         "two-dark", "blue-600", "#0c3992",
+
+        "three", "cyan-500", "#ffd800",
+        "three-light", "cyan-400", "#55b9e6",
         "three-dark", "cyan-600", "#0087b9",
+
         "red", "red-500", "#f01314",
+        "red-light", "red-400", "#347de0",
+        "red-dark", "red-600", "#c50014",
+
         "blue", "blue-500", "#1455c0",
-        "cyan", "cyan-500", "#ffd800",
+        "blue-light", "blue-400", "#347de0",
+        "blue-dark", "blue-600", "#0c3992",
+
         "yellow", "yellow-500", "#ffd800",
+        "yellow-light", "yellow-400", "#fff000",
+        "yellow-dark", "yellow-600", "#ffbb00",
+
+        "cyan", "cyan-500", "#ffd800",
+        "cyan-light", "cyan-400", "#55b9e6",
+        "cyan-dark", "cyan-600", "#0087b9",
+
         "green", "green-500", "#408335",
-        "teal", "turquoise-500", "#00a099",
+        "green-light", "green-400", "#66a558",
+        "green-dark", "green-600", "#2a7230",
+
+        "light-green", "light-green-500", "#63a615",
+        "light-green-light", "light-green-400", "#78be14",
+        "light-green-dark", "light-green-600", "#508b1b",
+
+        "turquoise", "turquoise-500", "#00a099",
+        "turquoise-light", "turquoise-400", "#3cb5ae",
+        "turquoise-dark", "turquoise-600", "#008984",
+
+        "gray", "cool-gray-500", "#646973",
+        "gray-light", "cool-gray-400", "#878C96",
+        "gray-dark", "cool-gray-600", "#3C414B",
+
         "pink", "pink-500", "#e93e8f",
-        "gray", "cool-gray-500", "#646973"
+        "pink-light", "pink-400", "#ee7bae",
+        "pink-dark", "pink-600", "#db0078"
     )
 
     values <- tbl$value
